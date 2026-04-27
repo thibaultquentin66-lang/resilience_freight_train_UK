91dd67c151397fd295809b9ea5c74063c4a8e210
thibaultquentin66-lang <thibault.quentin66@gmail.com>
2026-04-27 16:51:44 +0100

    Add files via upload

--- Tool.py
+++ Tool.py
@@ -1,14 +1,13 @@
 import pandas as pd
 import numpy as np
+import geopandas as gpd
+from shapely.wkt import loads
+import re
 
-
-
-#Clean column speed
+#Clean speed column
 def clean_speed(df):
-    # Convertir en string pour éviter les erreurs
     df["maxspeed"] = df["maxspeed"].astype(str)
 
-    # Nettoyer : enlever "mph", espaces, etc.
     df["maxspeed_clean"] = (
         df["maxspeed"]
         .str.lower()
@@ -16,16 +15,9 @@ def clean_speed(df):
         .str.strip()
     )
 
-    # Convertir en numérique (les valeurs impossibles deviennent NaN)
     df["maxspeed_clean"] = pd.to_numeric(df["maxspeed_clean"], errors="coerce")
-
-    # Remplir les NaN avec 60 mph
     df["maxspeed_clean"] = df["maxspeed_clean"].fillna(60)
-
-    # Remplacer l’ancienne colonne
     df["maxspeed"] = df["maxspeed_clean"]
-
-    # Supprimer la colonne temporaire
     df = df.drop(columns=["maxspeed_clean"])
 
     return df
@@ -61,3 +53,62 @@ def clean_elec(row):
         return 'Dual_System'
     else:
         return 'No'
+    
+#Transform a dateframe into a geodataframe
+def from_df_to_geodf(df, wkt_col='geometry_wkt', crs="EPSG:4326"):
+    df = df.copy()
+    df['geometry'] = df[wkt_col].apply(loads)
+    return gpd.GeoDataFrame(df, geometry='geometry', crs=crs)
+
+#Keep the higher loading gauge and turn it into a number
+#Example : some rows have a loading gauge like "W6, W8, W10" so the goal is to keep only W10
+def max_gauge(val):
+    if pd.isna(val) or val == '': return None
+    numbers = re.findall(r'\d+', str(val))
+    return max(map(int, numbers)) if numbers else None
+
+#Find rail neighbours using location to apply the same loading gauge for missing values
+def neighbour_gauge(gdf_rail, gdf_osm):
+
+    #Clean to avoid errors
+    cols_to_drop = ['index_right', 'index_left']
+    gdf_rail = gdf_rail.drop(columns=[c for c in cols_to_drop if c in gdf_rail.columns])
+    gauge_col = 'loading_gauge' if 'loading_gauge' in gdf_osm.columns else 'railway:loading_gauge'
+    gdf_osm['loading_gauge_val'] = gdf_osm[gauge_col].apply(max_gauge)
+    gdf_osm_clean = gdf_osm[['loading_gauge_val', 'geometry']].dropna(subset=['loading_gauge_val'])
+
+    #Join the datasets
+    joined = gpd.sjoin(gdf_rail, gdf_osm_clean, how='left', predicate='intersects')
+    joined = joined[~joined.index.duplicated(keep='first')]
+
+    #Propagation to the neighbours
+    inconnu = joined[joined['loading_gauge_val'].isna()].copy()
+    connu = joined[joined['loading_gauge_val'].notna()][['loading_gauge_val', 'geometry']]
+    
+    if not inconnu.empty and not connu.empty:
+        inconnu = inconnu.drop(columns=[c for c in cols_to_drop if c in inconnu.columns])
+        
+        #We create a buffer (a 1 meter zone) to ensure rail's connection
+        inconnu['geom_buffer'] = inconnu.geometry.buffer(0.00001) 
+        temp_inconnu = gpd.GeoDataFrame(inconnu, geometry='geom_buffer', crs=joined.crs)
+        
+        #We internaly join
+        spatial_hit = gpd.sjoin(temp_inconnu, connu, how='left', predicate='intersects')
+        
+        #We take back the loading gauge value
+        if 'loading_gauge_val_right' in spatial_hit.columns:
+            propagated = spatial_hit.groupby(spatial_hit.index)['loading_gauge_val_right'].max()
+            joined.loc[propagated.index, 'loading_gauge_val'] = joined.loc[propagated.index, 'loading_gauge_val'].fillna(propagated)
+        
+    return joined
+
+#Decide the loading gauge based on voltage if no neighbours were found
+def final_gauge(row):
+    """Décide de la valeur finale : OSM > Voltage > Défaut."""
+    if pd.notna(row['loading_gauge_val']):
+        return row['loading_gauge_val']
+    
+    volt = str(row.get('voltage', '')).lower()
+    if '25000' in volt or '25kv' in volt: return 10
+    if '750' in volt: return 7
+    return 6
